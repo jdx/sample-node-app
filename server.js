ab5b054bc22644c6d47908206c6ebc4aff8bcc6f
dickeyxxx <jeff@dickeyxxx.com>
2016-02-17 11:36:26 -0800

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var app     = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(process.env.PORT || 3000);
